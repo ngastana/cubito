43428d6ad73616733623da8ab917535864305468
Eneko Munoz <emunoz@c4r2s6.42urduliz.com>
2024-09-17 19:04:43 +0200

    falta gnl y render draw

--- game/get_images.c
+++ game/get_images.c
@@ -6,7 +6,7 @@
 /*   By: emunoz < emunoz@student.42urduliz.com >    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 10:35:50 by emunoz            #+#    #+#             */
-/*   Updated: 2024/09/17 17:53:21 by emunoz           ###   ########.fr       */
+/*   Updated: 2024/09/17 18:48:39 by emunoz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,20 @@ void	ft_mlx_xpm_file_to_img(void	**ptr, char *dir, t_game *cube)
 	*ptr = mlx_xpm_file_to_image(cube->mlx->mlx_ptr, dir, &res, &res);
 	if (!*ptr)
 	{
-		if (i == 0)
-			ft_destroy(cube);
 		if (i == 1)
-		{
 			mlx_destroy_image(cube->mlx->mlx_ptr, cube->mlx->north);
-			ft_destroy(cube);
-		}
 		if (i == 2)
 		{
 			mlx_destroy_image(cube->mlx->mlx_ptr, cube->mlx->north);
 			mlx_destroy_image(cube->mlx->mlx_ptr, cube->mlx->south);
-			ft_destroy(cube);
 		}
 		if (i == 3)
 		{
 			mlx_destroy_image(cube->mlx->mlx_ptr, cube->mlx->north);
 			mlx_destroy_image(cube->mlx->mlx_ptr, cube->mlx->south);
 			mlx_destroy_image(cube->mlx->mlx_ptr, cube->mlx->south);
-			ft_destroy(cube);
 		}
+		ft_destroy(cube);
 	}
 	i++;
 }
--- main.c
+++ main.c
@@ -6,11 +6,27 @@
 /*   By: emunoz < emunoz@student.42urduliz.com >    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 16:54:00 by ngastana          #+#    #+#             */
-/*   Updated: 2024/09/17 15:04:27 by emunoz           ###   ########.fr       */
+/*   Updated: 2024/09/17 19:04:05 by emunoz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube.h"
+
+void	ft_process_input(t_game *cube, int fd)
+{
+	ft_get_args(cube, fd);
+	close(fd);
+	if (ft_parse_assets(cube))
+	{
+		if (!ft_take_map(cube))
+			return (printf("Error4\n"), ft_clear_cub(cube), (void)0);
+		if (!ft_check_map_content(cube))
+			return (printf("Error5\n"), ft_clear_cub(cube), (void)0);
+		init_the_player(cube);
+		ft_start_game(cube);
+	}
+	ft_clear_cub(cube);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -21,7 +37,8 @@ int	main(int argc, char *argv[])
 	i = 0;
 	i = ft_strlen(argv[1]);
 	cube.file = NULL;
-	if (argc != 2 || (argv[1][i - 1] != 'b' || argv[1][i - 2] != 'u' || argv[1][i - 3] != 'c' || argv[1][i - 4] != '.'))
+	if (argc != 2 || (argv[1][i - 1] != 'b' || argv[1][i - 2] != 'u'
+		|| argv[1][i - 3] != 'c' || argv[1][i - 4] != '.'))
 		return (printf("Parameter bad written: ./cub3d <map>.cub\n"), 1);
 	else
 	{
@@ -31,17 +48,6 @@ int	main(int argc, char *argv[])
 		cube.file = malloc(500);
 		cube.map = malloc(500);
 		cube.mlx = malloc(sizeof(t_mlx));
-		ft_get_args(&cube, fd);
-		close(fd);
-		if (ft_parse_assets(&cube))
-		{
-			if (!ft_take_map(&cube))
-				return (printf("Error4\n"), ft_clear_cub(&cube), 0);
-			if (!ft_check_map_content(&cube))
-				return (printf("Error5\n"), ft_clear_cub(&cube), 0);
-			init_the_player(&cube);
-			ft_start_game(&cube);
-		}
-		ft_clear_cub(&cube);
+		ft_process_input(&cube, fd);
 	}
 }
